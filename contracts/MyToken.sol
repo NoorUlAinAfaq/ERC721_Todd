b425fef80b92c72054ed6f0a304d141a2553f5f5
NoorUlAinAfaq <noorulain.afaqkhatri@gmail.com>
2024-04-18 17:36:16 +0500

    increment added

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
     constructor(address initialOwner)
         ERC721("MyToken", "MTK")
         Ownable(initialOwner)
     {}
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://Todd/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
     }
 }
